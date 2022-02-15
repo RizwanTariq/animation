9710a93f4acba0368b328340ed3eca53cb084fd3
Rizwan Tariq <rizwantariq95@outlook.com>
2022-02-15 13:50:34 +0500

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button type="button" @click="flag = !flag">click</button>
+  <!-- <transition name="fade" mode="out-in">
+    <h1 v-if="flag" key="primary">Hello One!</h1>
+    <h1 v-else key="secondary">Hello Two!</h1>
+  </transition> -->
+  <!-- <transition name="zoom" appear>
+    <h1 v-if="flag">Hello!</h1>
+  </transition> -->
+
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+  >
+    <h1 v-if="flag">Hook</h1>
+  </transition>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      flag: true,
+    };
+  },
+  methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter", el);
+    },
+    enter(el, done) {
+      console.log("Enter", el);
+
+      const animate = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+        duration: 1000,
+      });
+
+      animate.onfinish = () => {
+        done();
+      };
+    },
+    afterEnter(el) {
+      console.log("afterEnter", el);
+    },
+    beforeLeave(el) {
+      console.log("beforeLeave", el);
+    },
+    leave(el, done) {
+      console.log("Leave", el);
+
+      const animate = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+        duration: 1000,
+      });
+
+      animate.onfinish = () => {
+        done();
+      };
+    },
+    afterLeave(el) {
+      console.log("afterLeave", el);
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: all 1s linear;
+}
+.fade-leave-active {
+  transition: all 1s linear;
+  opacity: 0;
+}
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+}
+.zoom-leave-active {
+  animation: zoom-out 1s linear;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
