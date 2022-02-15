8270d46dbb5b7b82e5fde5d5b07249724f43e603
Rizwan Tariq <rizwantariq95@outlook.com>
2022-02-15 16:21:30 +0500

    transition group.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" @click="flag = !flag">click</button>
+  <!-- <button type="button" @click="flag = !flag">click</button> -->
   <!-- <transition name="fade" mode="out-in">
     <h1 v-if="flag" key="primary">Hello One!</h1>
     <h1 v-else key="secondary">Hello Two!</h1>
@@ -8,7 +8,7 @@
     <h1 v-if="flag">Hello!</h1>
   </transition> -->
 
-  <transition
+  <!-- <transition
     @before-enter="beforeEnter"
     @enter="enter"
     @after-enter="afterEnter"
@@ -19,7 +19,19 @@
     name="fade"
   >
     <h1 v-if="flag">Hook</h1>
-  </transition>
+  </transition> -->
+  <button type="button" @click="addNumber">AddNumber</button>
+  <ul>
+    <transition-group name="fade">
+      <li
+        v-for="(number, index) in numbers"
+        :key="number"
+        @click="removeNumber(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+  </ul>
 </template>
 
 <script>
@@ -28,9 +40,19 @@ export default {
   data() {
     return {
       flag: true,
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
+    addNumber() {
+      this.numbers = [
+        ...this.numbers,
+        this.numbers[this.numbers.length - 1] + 1,
+      ];
+    },
+    removeNumber(index) {
+      this.numbers.splice(index, 1);
+    },
     beforeEnter(el) {
       console.log("beforeEnter", el);
     },
@@ -70,6 +92,13 @@ export default {
 </script>
 
 <style>
+li {
+  font-size: 20px;
+  cursor: pointer;
+}
+.fade-move {
+  transition: all 1s linear;
+}
 .fade-enter-from {
   opacity: 0;
 }
@@ -79,6 +108,7 @@ export default {
 .fade-leave-active {
   transition: all 1s linear;
   opacity: 0;
+  position: absolute;
 }
 .zoom-enter-active {
   animation: zoom-in 1s linear forwards;
